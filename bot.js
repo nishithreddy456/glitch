95b7a7e50da42a2616efaedc39411ad496b50b37
Glitch (green-rounded-elf) <none>
2020-04-25 15:23:40 +0000

    🍋🧀 Updated with Glitch

--- bot.js
+++ bot.js
@@ -1,105 +1,119 @@
-// packages
-const Telegraf = require('telegraf');
-const urlRegex = require('url-regex');
-const fetch = require('node-fetch');
-const ent = require('ent');
-const _ = require('lodash');
-const db = require('./db');
+const Telegraf = require('telegraf')
+const Markup = require("telegraf/markup");
+const Stage = require("telegraf/stage");
+const session = require("telegraf/session");
+const WizardScene = require("telegraf/scenes/wizard");
+const Extra=require('telegraf/extra')
+const loveCalculator = require("./loveCalculator");
 
-// init bot
-const { Extra, Markup } = Telegraf;
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.use(Telegraf.log())
 
-// regexs
-const titleRegex = /<title>(.+?)<\/title>/gim;
+bot.hears('markdown',ctx=>ctx.reply(
+`*bold text*
+_italic text_
+[inline URL](http://www.example.com/)
+[inline mention of a user](tg://user?id=123456789)
+\`inline fixed-width code\`
+\`\`\`block_language
+pre-formatted fixed-width code block
+\`\`\`
+`,Extra.markdown()));
+bot.command('html',ctx=>ctx.reply(`<b>bold</b>, <strong>bold</strong>
+<i>italic</i>, <em>italic</em>
+<u>underline</u>, <ins>underline</ins>
+<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
+<b>bold <i>italic bold <s>italic bold strikethrough</s> <u>underline italic bold</u></i> bold</b>
+<a href="https://loremflickr.com/320/240/paris">inline URL</a>
+<a href="tg://user?id=123456789">inline mention of a user</a>
+<code>inline fixed-width code</code>
+<pre>pre-formatted fixed-width code block</pre>`,Extra.HTML().webPreview(false)));
+bot.command('custom',ctx=>ctx.reply('keyboard',Markup.keyboard(
+[['Dog','Cat'],
+['Bear','Monkey','Bird']])
+                                   .resize()
+                                   .extra()))
+bot.hears('Dog',ctx=>ctx.reply(`<a href="${random('dog')}">DOG</a>`,Extra.HTML()));
+bot.hears('Cat',ctx=>ctx.reply(`<a href="${random('cat')}">CAT</a>`,Extra.HTML()));
+bot.hears('Bear',ctx=>ctx.reply(`<a href="${random('bear')}">Bear</a>`,Extra.HTML()));
+bot.hears('Monkey',ctx=>ctx.reply(`<a href="${random('monkey')}">Mokey</a>`,Extra.HTML()));
+bot.hears('Bird',ctx=>ctx.reply(`<a href="${random('bird')}">Bird</a>`,Extra.HTML()));
+const random=tag=>{
+  let imgId=Math.trunc(Math.random()*1000)
+  let url=`https://loremflickr.com/320/240/${tag}/?lock=${imgId}`
+  return url
+} 
+//Inline keyboard
+bot.command('inline',ctx=>ctx.reply(`Random image <a href="${random('dog')}">DOG</a>`,
+                                    Extra.HTML().markup((m)=>m.inlineKeyboard([m.callbackButton('Next Image','dog'),
+                                                                              m.callbackButton('Next Image','do')]))));
+bot.action('dog',ctx=>ctx.editMessageText(`Random image <a href="${random('dog')}">DOG</a>`,
+                                    Extra.HTML().markup((m)=>m.inlineKeyboard([m.callbackButton('Next Image','dog')]))))
 
-// memory storage
-const memoryStore = {};
 
-// categories list
-const categories = ['Articles & News', 'Releases', 'Libs & Demos', 'Silly stuff'];
-
-// helper functions
-const sanitizeTitle = title => title.replace(/[\n\r\t]+/gm, ' ');
-const generateMarkdown = async (userId) => {
-  const {links} = await db.findOne({userId});
-  
-  const groupedLinks = _.groupBy(links, it => it.category);
-  
-  const markdown = categories.map(category => {
-    const header = `\n## ${category}\n`
-    const links = groupedLinks[category] ? groupedLinks[category].map(link => `- [${sanitizeTitle(link.title)}](${link.url})\n`).reduce((acc, val) => acc + val, '') : ' - No links yet\n';
-    return `${header}${links}`;
-  }).reduce((acc, val) => acc + val, '');
-  
-  return markdown;
-};
-
-// basic messages
-const welcomeMessage = `Welcome! 
-I am devkd Telegram Bot.
-Send me your links to store them.`;
-const helpMessage = `Send me a link`;
-
-// basic commands
-bot.start((ctx) => ctx.reply(welcomeMessage));
-bot.help((ctx) => ctx.reply(helpMessage));
-
-// listen to commands
-bot.hears(/new collection (.+)/, async (ctx) => {
-  const userId = ctx.from.id;
-  const collectionName = ctx.match[1];
-  
-  // remove all old collection
-  await db.remove({userId}, {multi: true});
-  
-  // create new
-  await db.insert({userId, collectionName, links: []});
-  
-  ctx.reply(`New collection created with name: ${collectionName}`);
-});
-bot.hears(urlRegex(), async (ctx) => {
-  // get urls from message text
-  const urls = ctx.message.text.match(urlRegex());
-  const firstUrl = urls[0];
-  
-  // get url title
-  const body = await fetch(firstUrl).then(r => r.text());
-  const titleTag = body.match(titleRegex);
-  const title = ent.decode(titleTag.pop().replace('<title>', '').replace('</title>', ''));
-  
-  // get userId
-  const userId = ctx.from.id;
-  memoryStore[userId] = {url: firstUrl, title, category: ''};
-  
-  ctx.reply(`Ready to save: "${title}".
-What category should it be?`, Markup.keyboard(categories).oneTime().resize().extra());
-});
-// category handling
-categories.forEach(category => {
-  bot.hears(category, async (ctx) => {
-    const userId = ctx.from.id;
-    const linkObject = memoryStore[userId]; // {url: firstUrl, title, category: ''};
-    linkObject.category = category;
-
-    // find current collection
-    await db.update({userId}, {$push: {links: linkObject}});
-
-    ctx.reply(`Saved link into ${category}: ${memoryStore[userId].title}`);
-  });
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('modern', ({ reply }) => reply('Yo'))
+bot.command('hipster', Telegraf.reply('λ'))
+//bot.on('text',ctx=>{
+  //ctx.reply('Hello')
+  //console.log('update\n',ctx.update)
+  //console.log("from\n",ctx.from)
+//});
+bot.command('check',(ctx) => {
+  ctx.reply(
+    `Hello ${ctx.from.first_name}, would you like to know the love compatibility?`,
+    Markup.inlineKeyboard([
+      Markup.callbackButton("Love Calculate", "LOVE_CALCULATE")
+    ]).extra()
+  );
 });
 
-bot.hears(/generate markdown/i, async (ctx) => {
-  const userId = ctx.from.id;  
-  const markdown = await generateMarkdown(userId);
-  ctx.reply(markdown, Extra.webPreview(false));
-});
+// love calculator two-step wizard
+const loveCalculate = new WizardScene(
+  "love_calculate",
+  ctx => {
+    ctx.reply("Please, enter your name"); // enter your name
+    return ctx.wizard.next();
+  },
+  ctx => {
+    ctx.wizard.state.yourName = ctx.message.text; // store yourName in the state to share data between middlewares
+    ctx.reply(
+      "Enter the name of your partner/lover/crush to find Love compatibility & chances of successful love relationship."
+    );
+    return ctx.wizard.next();
+  },
+  ctx => {
+    const partnerName = ctx.message.text; // retrieve partner name from the message which user entered
+    const yourName = ctx.wizard.state.yourName; // retrieve your name from state
+    loveCalculator
+      .getPercentage(yourName, partnerName)
+      .then(res => {
+        const { fname, sname, percentage, result } = res.data;
+        ctx.reply(
+          `${fname} + ${sname} = ${percentage}% \n ${percentage > 50 ? '☺️' : '😢'} ${result}`,
+          Markup.inlineKeyboard([
+            Markup.callbackButton(
+              "♥️ calculate Another Relationship",
+              "LOVE_CALCULATE"
+            )
+          ]).extra()
+        );
+      })
+      .catch(err => ctx.reply(
+        err.message,
+        Markup.inlineKeyboard([
+          Markup.callbackButton("calculate again", "LOVE_CALCULATE")
+        ]).extra()
+      ));
+    return ctx.scene.leave();
+  }
+);
 
-bot.hears(/generate preview/i, async (ctx) => {
-  const userId = ctx.from.id;  
-  const markdown = await generateMarkdown(userId);
-  ctx.reply(markdown, Extra.markdown().webPreview(false));
-});
-
-// start bot
-bot.startPolling();
+const stage = new Stage([loveCalculate], { default: "love_calculate" }); // Scene registration
+bot.use(session());
+bot.use(stage.middleware());
+bot.launch()
